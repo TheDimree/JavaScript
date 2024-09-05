baf920fd5f191d44ada305b44f578f85cebd2ee8
Bhagesh Dimri <TheDimree@gmail.com>
2024-09-05 22:05:17 +0530

    index3.js completed

--- Redux/index2.js
+++ Redux/index2.js
@@ -1,20 +1,26 @@
-//Action Creator
+//Action Creators, make action name constant
 
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'; //   download a middleware: npm i redux-logger
 
+//* Action name constants = remove string error
+const increment='increment';
+const decrement='decrement';
+const incrementByAmount='incrementByAmount';
+
 //store
 const store = createStore(reducer, applyMiddleware(logger.default));
 
+
 // reducer changes and return the state after applying the action
 function reducer(state={amount:100}, action) {   //parameter order is fixed and always returns a state
-    if(action.type==='increment') {
+    if(action.type===increment) {
         return {amount: state.amount+1} // immutably return new state copy
     }
-    if(action.type==='decrement') {
+    if(action.type=== decrement) {
         return {amount: state.amount-1} // immutably return new state copy
     }
-    if(action.type==='incrementByAmount') {
+    if(action.type===incrementByAmount) {
         return {amount: state.amount+action.payload} // immutably return new state copy
     }
     return state;
@@ -24,19 +30,19 @@ function reducer(state={amount:100}, action) {   //parameter order is fixed and 
 // console.log(store.getState());
 
 
-//* Action Creator
-function increment() {
-    return {type:'increment'}
+//* Action Creators
+function inc() {
+    return {type:increment}
 }
-function decrement() {
-    return {type:'decrement'}
+function dec() {
+    return {type:decrement}
 }
-function incrementByAmount(value) {
-    return {type:'incrementByAmount', payload:value}
+function incByAmt(value) {
+    return {type:incrementByAmount, payload:value}
 }
 
 setInterval(()=>{
-    // store.dispatch(increment()) // store.dispatch({type: 'increment'})
-    // store.dispatch(decrement()) // store.dispatch({type: 'decrement'}) 
-    store.dispatch(incrementByAmount(5)) // store.dispatch({type: 'incrementByAmount', payload:4})
+    // store.dispatch(inc()) // store.dispatch({type: 'increment'})
+    // store.dispatch(dec()) // store.dispatch({type: 'decrement'}) 
+    store.dispatch(incByAmt(5)) // store.dispatch({type: 'incrementByAmount', payload:4})
 },2000)
